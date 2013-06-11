599cc9314b1cea329fc150ba73101ddcc2713538
Tomasz Adamski <tomasz.adamski@gmail.com>
2013-06-11 23:31:12 +0200

    work logs cached, refactoring & cleaning

--- src/background.js
+++ src/background.js
@@ -1,62 +1,79 @@
+var reloadLoggedWorkEvents;
+(function() {
+    chrome.alarms.create("refresh", {"periodInMinutes": 5, "delayInMinutes": 5});
+    chrome.alarms.onAlarm.addListener(refreshData);
 
-chrome.alarms.create("refresh",{"periodInMinutes":1/6, "delayInMinutes":1/60});
-chrome.alarms.onAlarm.addListener(function(alarm) {
-	parseLoggedTime(function(data){
-		var total = data.reduce(
-			function(curr,prev){
-				return {loggedWork: curr.loggedWork+prev.loggedWork};
-			},{loggedWork:0}).loggedWork;
-		var totalStr = Math.floor(total/60)+":"+pad(total%60,2);;
-		chrome.browserAction.setBadgeText({"text":totalStr});
-	});
-});
+    function updateTotalActionBadge(loggedWorkEvents) {
+        var total = loggedWorkEvents.reduce(function(curr, prev) {
+            return {loggedWork: curr.loggedWork + prev.loggedWork};
+        }, {loggedWork: 0}).loggedWork;
+        var totalStr = Math.floor(total / 60) + ":" + pad(total % 60, 2);
+        chrome.browserAction.setBadgeText({"text": totalStr});
+    }
 
-function parseLoggedTime(callback){
-	var startDate = new Date();
-	startDate.setHours(0,0,0,0);
-	var endDate = new Date(); 
-	endDate.setDate(startDate.getDate()+1);
-	endDate.setHours(0,0,0,0);
-	chrome.storage.local.get(function(data){
-		if(data.username && data.server && data.key){
-		$.get(data.server+"/activity?maxResults=99&streams=key+IS+"+data.key+"&streams=update-date+BETWEEN+"+startDate.getTime()+"+"+endDate.getTime()+"&streams=user+IS+"+data.username+"&issues=activity+IS+issue%3Aupdate&providers=issues&os_authType=basic",
-		function(data) {
-			var $xml = $(data); 
-			var logEntries = [];
-			$xml.find("entry").each(function(){
-				var $this=$(this);
-				var matchesMultipleChanges = $this.find("content[type=html]").text().match(/.*<li>Logged '(.*)'<\/li>.*/);
-				var matchesSingleChange = $this.find("title[type=html]").text().match(/.*logged '(.*)' on.*/);
-				var matches = matchesMultipleChanges ? matchesMultipleChanges : matchesSingleChange;
-				if(matches){
-					logEntries.push(
-						{
-							"key":$this.find("title[type=text]").text(),
-							"summary":$this.find("summary[type=text]").text(),
-							"link":$this.find("link[rel=alternate]").attr("href"),
-							"loggedWork": loggedTimeInMinutes(matches[1]),
-							"date": new Date($this.find("published").text())
-						}
-					);
-					
-				}
-			});
-			callback(logEntries);
-		}
-		);
-		}
-	});
-	
-}
+    function storeEventsInStorage(loggedWorkEvents) {
+        chrome.storage.local.set({
+            'loggedWorkEvents': loggedWorkEvents
+        });
+    }
 
-function loggedTimeInMinutes(loggedWork){
-	var e = loggedWork.replace(/h/g, "*60");
-	e = e.replace(/m/g, "");
-	e = e.replace(/d/g, "*60*8");
-	e = e.replace(/ /g, "+");
-	return eval(e);
-}
+    function refreshData() {
+        var loggedWorkEvents = getLoggedWorkEvents();
+        loggedWorkEvents.done(updateTotalActionBadge);
+        loggedWorkEvents.done(storeEventsInStorage);
+    }
+    reloadLoggedWorkEvents = refreshData;
 
-function pad (str, max) {
-	return (str+"").length < max ? pad("0" + str, max) : str;
-}
+    function getLoggedWorkEvents() {
+        var deferred = new $.Deferred();
+        var startDate = new Date();
+        startDate.setHours(0, 0, 0, 0);
+        var endDate = new Date();
+        endDate.setDate(startDate.getDate() + 1);
+        endDate.setHours(0, 0, 0, 0);
+        chrome.storage.local.get(function(data) {
+            if (data.username && data.server && data.key) {
+                $.get(data.server + "/activity?maxResults=99&streams=key+IS+" + data.key + "&streams=update-date+BETWEEN+" + startDate.getTime() + "+" + endDate.getTime() + "&streams=user+IS+" + data.username + "&issues=activity+IS+issue%3Aupdate&providers=issues&os_authType=basic",
+                        function(data) {
+                            var $xml = $(data);
+                            var logEntries = [];
+                            $xml.find("entry").each(function() {
+                                var $this = $(this);
+                                var matchesMultipleChanges = $this.find("content[type=html]").text().match(/.*<li>Logged '(.*)'<\/li>.*/);
+                                var matchesSingleChange = $this.find("title[type=html]").text().match(/.*logged '(.*)' on.*/);
+                                var matches = matchesMultipleChanges ? matchesMultipleChanges : matchesSingleChange;
+                                if (matches) {
+                                    logEntries.push(
+                                            {
+                                                "key": $this.find("title[type=text]").text(),
+                                                "summary": $this.find("summary[type=text]").text(),
+                                                "link": $this.find("link[rel=alternate]").attr("href"),
+                                                "loggedWork": loggedTimeInMinutes(matches[1]),
+                                                "date": new Date($this.find("published").text()).getTime()
+                                            }
+                                    );
+
+                                }
+                            });
+                            deferred.resolve(logEntries);
+                        }
+                );
+            }
+        });
+        return deferred;
+    }
+
+
+
+    function loggedTimeInMinutes(loggedWork) {
+        var e = loggedWork.replace(/h/g, "*60");
+        e = e.replace(/m/g, "");
+        e = e.replace(/d/g, "*60*8");
+        e = e.replace(/ /g, "+");
+        return eval(e);
+    }
+
+    function pad(str, max) {
+        return (str + "").length < max ? pad("0" + str, max) : str;
+    }
+})();
--- src/popup.js
+++ src/popup.js
@@ -1,18 +1,32 @@
-		$(function(){
-			parseLoggedTime(function(data){
-				var odd = false;
-				data.forEach(function(item){
-					$('table tbody:last').prepend("<tr class='"+(odd?"odd":"")+"'><td class='summary'><a target='_blank' href='"+item.link+"' title='"+item.summary+"'>"+item.key+": "+item.summary+"</a></td><td>"+item.date.getHours()+":"+pad(item.date.getMinutes(),2)+"</td><td>"+ Math.floor(item.loggedWork/60)+":"+pad(item.loggedWork%60, 2)+"</td></tr>")
-					odd=!odd;
-				});
-				var total = data.reduce(
-				function(curr,prev){
-					return {loggedWork: curr.loggedWork+prev.loggedWork};
-				},{loggedWork:0}).loggedWork;
-				$("table tfoot td:last").text(Math.floor(total/60)+":"+pad(total%60, 2));
-			});
-			function pad (str, max) {
-				return (str+"").length < max ? pad("0" + str, max) : str;
-			}
-		});
+$(function() {
+    chrome.storage.onChanged.addListener(function(changes) {
+        if (changes.loggedWorkEvents) {
+            renderHtml(changes.loggedWorkEvents.newValue);
+        }
+    });
+    chrome.storage.local.get(function(data) {
+        renderHtml(data.loggedWorkEvents)
+    });
+    chrome.extension.getBackgroundPage().reloadLoggedWorkEvents();
+
+    function renderHtml(loggedWorkEvents) {
+        var odd = false;
+        $('table tbody:last').empty();
+        loggedWorkEvents.forEach(function(item) {
+            var date = new Date(item.date)
+            $('table tbody:last').prepend("<tr class='" + (odd ? "odd" : "") + "'><td class='summary'><a target='_blank' href='" + item.link + "' title='" + item.summary + "'>" + item.key + ": " + item.summary + "</a></td><td>" + date.getHours() + ":" + pad(date.getMinutes(), 2) + "</td><td>" + Math.floor(item.loggedWork / 60) + ":" + pad(item.loggedWork % 60, 2) + "</td></tr>")
+            odd = !odd;
+        });
+        var total = loggedWorkEvents.reduce(
+                function(curr, prev) {
+                    return {loggedWork: curr.loggedWork + prev.loggedWork};
+                }, {loggedWork: 0}).loggedWork;
+        $("table tfoot td:last").text(Math.floor(total / 60) + ":" + pad(total % 60, 2));
+    }
+
+    function pad(str, max) {
+        return (str + "").length < max ? pad("0" + str, max) : str;
+    }
+
+});
 		
